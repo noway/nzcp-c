d219be762cce403ef3b55a6efc88b2efd64c116f
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2021-12-05 02:59:07 +1300

    switch to more concise types

--- main.c
+++ main.c
@@ -4,19 +4,19 @@
 #include "base32.h"
 #include <tinycbor/cbor.h>
 
-const unsigned char *EXAMPLE_PASS =
-  (unsigned char *) "NZCP:/1/2KCEVIQEIVVWK6JNGEASNICZAEP2KALYDZSGSZB2O5SWEOTOPJRXALTDN53GSZBRHEXGQZLBNR2GQLTOPICRUYMBTIFAIGTUKBAAUYTWMOSGQQDDN5XHIZLYOSBHQJTIOR2HA4Z2F4XXO53XFZ3TGLTPOJTS6MRQGE4C6Y3SMVSGK3TUNFQWY4ZPOYYXQKTIOR2HA4Z2F4XW46TDOAXGG33WNFSDCOJONBSWC3DUNAXG46RPMNXW45DFPB2HGL3WGFTXMZLSONUW63TFGEXDALRQMR2HS4DFQJ2FMZLSNFTGSYLCNRSUG4TFMRSW45DJMFWG6UDVMJWGSY2DN53GSZCQMFZXG4LDOJSWIZLOORUWC3CTOVRGUZLDOSRWSZ3JOZSW4TTBNVSWISTBMNVWUZTBNVUWY6KOMFWWKZ2TOBQXE4TPO5RWI33CNIYTSNRQFUYDILJRGYDVAYFE6VGU4MCDGK7DHLLYWHVPUS2YIDJOA6Y524TD3AZRM263WTY2BE4DPKIF27WKF3UDNNVSVWRDYIYVJ65IRJJJ6Z25M2DO4YZLBHWFQGVQR5ZLIWEQJOZTS3IQ7JTNCFDX";
+const uint8_t *EXAMPLE_PASS =
+  (uint8_t *) "NZCP:/1/2KCEVIQEIVVWK6JNGEASNICZAEP2KALYDZSGSZB2O5SWEOTOPJRXALTDN53GSZBRHEXGQZLBNR2GQLTOPICRUYMBTIFAIGTUKBAAUYTWMOSGQQDDN5XHIZLYOSBHQJTIOR2HA4Z2F4XXO53XFZ3TGLTPOJTS6MRQGE4C6Y3SMVSGK3TUNFQWY4ZPOYYXQKTIOR2HA4Z2F4XW46TDOAXGG33WNFSDCOJONBSWC3DUNAXG46RPMNXW45DFPB2HGL3WGFTXMZLSONUW63TFGEXDALRQMR2HS4DFQJ2FMZLSNFTGSYLCNRSUG4TFMRSW45DJMFWG6UDVMJWGSY2DN53GSZCQMFZXG4LDOJSWIZLOORUWC3CTOVRGUZLDOSRWSZ3JOZSW4TTBNVSWISTBMNVWUZTBNVUWY6KOMFWWKZ2TOBQXE4TPO5RWI33CNIYTSNRQFUYDILJRGYDVAYFE6VGU4MCDGK7DHLLYWHVPUS2YIDJOA6Y524TD3AZRM263WTY2BE4DPKIF27WKF3UDNNVSVWRDYIYVJ65IRJJJ6Z25M2DO4YZLBHWFQGVQR5ZLIWEQJOZTS3IQ7JTNCFDX";
 
   
 #define mmalloc(size) calloc(1, size)
 
-unsigned long next_token_len(const unsigned char *uri, unsigned long skip_pos) {
+size_t next_token_len(const uint8_t *uri, size_t skip_pos) {
   // TODO: don't need mmalloc
   char *str_copy = mmalloc(strlen((char*) uri) + 1);
   strcpy(str_copy, (char*) uri);
   char *skipped_str_copy = (char*) (str_copy + skip_pos);
   char *token = strtok(skipped_str_copy, "/");
-  unsigned long token_len = strlen(token);
+  size_t token_len = strlen(token);
   free(str_copy);
   return token_len;
 }
@@ -24,13 +24,13 @@ unsigned long next_token_len(const unsigned char *uri, unsigned long skip_pos) {
 int main(void) {
   // TODO: check for every CborError and return error code
 
-  unsigned long token1_len = next_token_len(EXAMPLE_PASS, 0);
-  unsigned long token2_len = next_token_len(EXAMPLE_PASS, token1_len + 1);
-  unsigned long token3_len = next_token_len(EXAMPLE_PASS, token1_len + 1 + token2_len + 1);
+  size_t token1_len = next_token_len(EXAMPLE_PASS, 0);
+  size_t token2_len = next_token_len(EXAMPLE_PASS, token1_len + 1);
+  size_t token3_len = next_token_len(EXAMPLE_PASS, token1_len + 1 + token2_len + 1);
 
-  const unsigned char* payload_prefix = EXAMPLE_PASS;
-  const unsigned char* version_identifier = EXAMPLE_PASS + token1_len + 1;
-  const unsigned char* base32_encoded_cwt = EXAMPLE_PASS + token1_len + 1 + token2_len + 1;
+  const uint8_t* payload_prefix = EXAMPLE_PASS;
+  const uint8_t* version_identifier = EXAMPLE_PASS + token1_len + 1;
+  const uint8_t* base32_encoded_cwt = EXAMPLE_PASS + token1_len + 1 + token2_len + 1;
 
   // TODO: check payload prefix and version identifier
   printf("payload_prefix %s %lu\n", payload_prefix, token1_len);
@@ -39,7 +39,7 @@ int main(void) {
   
   // TODO: add base32 padding
   size_t binary_cwt_max = strlen((char*) base32_encoded_cwt) + 1; // TODO: FIX: this is the length of stringified base32, not the binary length
-  unsigned char *binary_cwt = mmalloc(binary_cwt_max);
+  uint8_t *binary_cwt = mmalloc(binary_cwt_max);
   base32_decode(base32_encoded_cwt, binary_cwt);
   size_t binary_cwt_len = strlen((char*) binary_cwt);
   printf("binary_cwt %s \n", binary_cwt);
