af3c2a8b83fa447899eb13fa75467850ac8f9fd6
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2021-12-06 00:57:57 +1300

    verification of signature

--- main.c
+++ main.c
@@ -14,6 +14,20 @@ char* PUBLIC_KEY_Y = "Iv5SU6FuW-TRYh5_GOrJlcV_gpF_GpFQhCOD8LSk3T0";
 char* TRUSTED_ISSUER = "did:web:nzcp.covid19.health.nz";
 
 
+
+static const sb_sw_public_t TEST_PUB_2 = {
+    {
+        0x60, 0xFE, 0xD4, 0xBA, 0x25, 0x5A, 0x9D, 0x31,
+        0xC9, 0x61, 0xEB, 0x74, 0xC6, 0x35, 0x6D, 0x68,
+        0xC0, 0x49, 0xB8, 0x92, 0x3B, 0x61, 0xFA, 0x6C,
+        0xE6, 0x69, 0x62, 0x2E, 0x60, 0xF2, 0x9F, 0xB6,
+        0x79, 0x03, 0xFE, 0x10, 0x08, 0xB8, 0xBC, 0x99,
+        0xA4, 0x1A, 0xE9, 0xE9, 0x56, 0x28, 0xBC, 0x64,
+        0xF2, 0xF1, 0xB2, 0x0C, 0x2D, 0x7E, 0x9F, 0x51,
+        0x77, 0xA3, 0xC2, 0x94, 0xD4, 0x46, 0x22, 0x99
+    }
+};
+
 void* mmalloc(size_t size) {
   void* ptr = malloc(size);
   memset(ptr, 0, size);
@@ -334,16 +348,29 @@ int main(void) {
   printf("tobe_signed_buflen_actual: %zu\n", tobe_signed_buflen_actual);
 
   sb_sha256_state_t *sha256_state = mmalloc(sizeof(struct sb_sha256_state_t)); // TODO: put on stack?
-  sb_byte_t *output = mmalloc(sizeof(sb_byte_t)); // TODO: put on stack?
+  sb_byte_t *msg_hash = mmalloc(sizeof(sb_byte_t)); // TODO: put on stack?
+  size_t msg_hash_len = sizeof(sb_byte_t);
 
-  sb_sha256_message(sha256_state, output, tobe_signed_buf, tobe_signed_buflen_actual);
+  sb_sha256_message(sha256_state, msg_hash, tobe_signed_buf, tobe_signed_buflen_actual);
 
-  printf("output: %02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x\n", *output, *(output+1), *(output+2), *(output+3), *(output+4), *(output+5), *(output+6), *(output+7), *(output+8), *(output+9), *(output+10), *(output+11), *(output+12), *(output+13), *(output+14), *(output+15), *(output+16), *(output+17), *(output+18), *(output+19), *(output+20), *(output+21), *(output+22), *(output+23), *(output+24), *(output+25), *(output+26), *(output+27), *(output+28), *(output+29), *(output+30), *(output+31));
+  printf("msg_hash: %02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x\n", *msg_hash, *(msg_hash+1), *(msg_hash+2), *(msg_hash+3), *(msg_hash+4), *(msg_hash+5), *(msg_hash+6), *(msg_hash+7), *(msg_hash+8), *(msg_hash+9), *(msg_hash+10), *(msg_hash+11), *(msg_hash+12), *(msg_hash+13), *(msg_hash+14), *(msg_hash+15), *(msg_hash+16), *(msg_hash+17), *(msg_hash+18), *(msg_hash+19), *(msg_hash+20), *(msg_hash+21), *(msg_hash+22), *(msg_hash+23), *(msg_hash+24), *(msg_hash+25), *(msg_hash+26), *(msg_hash+27), *(msg_hash+28), *(msg_hash+29), *(msg_hash+30), *(msg_hash+31));
 
-  // sb_sw_context_t *context = mmalloc(sizeof(sb_sw_context_t));
-  // extern sb_error_t sb_sw_verify_signature_sha256(context, sb_sw_message_digest_t digest[static 1], const sb_sw_signature_t signature[static 1], 
-  //   const sb_sw_public_t public[static 1], const sb_byte_t* input, size_t input_len, sb_hmac_drbg_state_t* drbg, sb_sw_curve_id_t curve, sb_data_endian_t e);
+  sb_sw_context_t *context = mmalloc(sizeof(sb_sw_context_t));
+  sb_sw_message_digest_t *dig = mmalloc(sizeof(sb_sw_message_digest_t));
+  sb_error_t error = sb_sw_verify_signature_sha256(context, dig, signature, &TEST_PUB_2, msg_hash, msg_hash_len, NULL, SB_SW_CURVE_P256, SB_DATA_ENDIAN_BIG);
 
+  printf("error: %d\n", error);
+  printf("SB_ERROR_SIGNATURE_INVALID: %d\n", SB_ERROR_SIGNATURE_INVALID);
+
+  if (error == SB_SUCCESS) {
+    printf("Result: NZ COVID Pass is valid\n");
+  }
+  else if (error == SB_ERROR_SIGNATURE_INVALID) {
+    printf("Result: NZ COVID Pass is invalid. Invalid signature.\n");
+  }
+  else {
+    printf("Result: NZ COVID Pass is invalid. Unkown error.\n");
+  }
 
   free(binary_cwt);
   free(protected);
