10095935622024e8c992e8aa556d8ef03622907e
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2021-12-30 00:00:30 +1300

    print pass in JNI

--- jni/NZCPJNI.cpp
+++ jni/NZCPJNI.cpp
@@ -2,7 +2,9 @@
 #include <jni.h>
 #include "NZCPJNI.h"
 
-JNIEXPORT jint JNICALL Java_NZCPJNI_verify_1pass_1uri (JNIEnv *, jobject, jstring) {
-    printf("Hello from nzcpjni");
+JNIEXPORT jint JNICALL Java_NZCPJNI_verify_1pass_1uri (JNIEnv * env, jobject self, jstring pass_uri) {
+    const char *pass_uri_native = env->GetStringUTFChars(pass_uri, 0);
+    printf("Hello from nzcpjni %s", pass_uri_native);
+    env->ReleaseStringUTFChars(pass_uri, pass_uri_native);
     return 0;
 }
